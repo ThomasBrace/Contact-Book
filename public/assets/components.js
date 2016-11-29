075c5b463980f23272fc4d9ee251628fa4edae1e
Thomas Brace <thomas.idris.brace@googlemail.com>
2016-11-29 14:50:57 +0000

    basic setup with npm

--- public/assets/components.js
+++ public/assets/components.js
@@ -1,32 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="description" content="">
-    <meta name="author" content="Thomas Brace">
-
-    <title>Contact Book</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-
-      <div id="app">
-      </div>
-
-      <script src="https://fb.me/react-15.0.0.js"></script>
-      <script src="https://fb.me/react-dom-15.0.0.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js"></script>
-      <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-      <link rel="stylesheet" type="text/css" href="style.css">
-
-      <script type="text/babel">
-
-            class AppContainer extends React.Component { //parent Component
+class AppContainer extends React.Component { //parent Component
               constructor() {
                 super();
 
@@ -294,8 +266,5 @@
 
 
             ReactDOM.render(
-              <AppContainer />, document.getElementById('app')
+              <AppContainer />, document.getElementById('app'), function(){console.timeEnd("app")}
             );
-      </script>
-   </body>
-</html>
